477c85ac5a9da2b314d3edf1393282a83b46c29b
user <user@DESKTOP-7AK0NU9>
2023-09-19 04:12:33 +0300

    edited

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -5,18 +5,18 @@ namespace E_commerce.Controllers
 {
     public class CartController : Controller
     {
-        /*private readonly CartServies _CartService;
+       /* private readonly CartServies _CartService;
         private readonly ProductServices _productService; // You may have a ProductService to manage products.
 
         public CartController(CartServies CartService, ProductServices productService)
         {
             _CartService = CartService;
             _productService = productService;
-        }
+        }*/
         public IActionResult Index()
         {
             return View();
         }
-    }*/
     }
-}
+    }
+
