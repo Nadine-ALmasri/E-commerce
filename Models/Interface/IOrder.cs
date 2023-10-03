1f18ec032e9edecd81389503493a4df6897ec980
Mhamad Belal Al Msalma <117995558+MhamadBelal@users.noreply.github.com>
2023-10-04 00:39:14 +0300

    order changes

---
+++ Models/Interface/IOrder.cs
@@ -0,0 +1,9 @@
+﻿using E_commerce.Models.DTOs;
+
+namespace E_commerce.Models.Interface
+{
+    public interface IOrder
+    {
+        public Task<List<Order>> GetAllOrders();
+    }
+}
---
+++ Models/Services/OrderServices.cs
@@ -0,0 +1,21 @@
+﻿using E_commerce.Data;
+using E_commerce.Models.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Models.Services
+{
+    public class OrderServices : IOrder
+    {
+        private readonly E_commerceDbContext _context;
+
+        public OrderServices(E_commerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Order>> GetAllOrders()
+        {
+            var Orders= await _context.Orders.Include(c=>c.ShoppingCart).ToListAsync();
+            return Orders;
+        }
+    }
+}
